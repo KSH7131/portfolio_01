5e0f0e26106a23e3198d902f6ba1e056701913bd
ksh7131 <gimsh7131@gmail.com>
2024-09-27 12:03:23 +0900

    240927_third

--- data/navi.js
+++ data/navi.js
@@ -13,11 +13,13 @@ const navidata = [
 ,
   {
     d1text:"50호점 한정 혜택",
-    d1href:"https://naver.com"
+    d1href:"https://naver.com",
+    target : ""
 }
 ,
   {
     d1text:"가맹 문의",
-    d1href:"https://naver.com"
+    d1href:"https://naver.com",
+    target : "_blank"
 }
 ];
--- ksh/ksh.js
+++ ksh/ksh.js
@@ -1,42 +1,17 @@
-// window.addEventListener("load", function(){
-// const mynavi =  this.document.querySelectorAll("#navmenu li");
-//   // 배열형 데이터
+  window.addEventListener("load", function(){
+  const mynavi =  this.document.querySelector("#navmenu ul");
 
-//   mynavi[1].innerHTML = 
-//   `<a href="${navidata[0].d1href}">
-//   ${navidata[0].d1text}
-//   </a>
-//   `;
+  let mytag = '';
 
-//   mynavi[2].innerHTML = 
-//   `<a href="${navidata[1].d1href}">
-//   ${navidata[1].d1text}
-//   </a>
-//   `;
-
-//   mynavi[3].innerHTML = 
-//   `<a href="${navidata[2].d1href}">
-//   ${navidata[2].d1text}
-//   </a>
-//   `;
-
-//   mynavi[4].innerHTML = 
-//   `<a href="${navidata[3].d1href}">
-//   ${navidata[3].d1text}
-//   </a>
-//   `;
-
-
-// })
-
-window.addEventListener("load", function(){
-  const mynavi = this.document.querySelectorAll("#navmenu li");
-  
-  for (let i = 0; i < 4; i++) {
-    mynavi[i+1].innerHTML = 
-    `<a href="${navidata[i].d1href}">
-    ${navidata[i].d1text}
+  for(x in navidata){
+    mytag += `<li>
+    <a
+    href="${navidata[x].d1href}"
+    target="${navidata[x].__target}">
+    ${navidata[x].d1text}
     </a>
-    `;
+    </li>`;
   }
-});
+
+  mynavi.innerHTML = mytag;
+})
